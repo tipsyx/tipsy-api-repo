062acffdbca4d3a8604b14bd72cee139bfaf2873
tipsyx <141881555+tipsyx@users.noreply.github.com>
2023-09-15 18:39:40 +0100

    Update project.py

--- api/project.py
+++ api/project.py
@@ -11,22 +11,6 @@ class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer)
-    gender = db.Column(db.String(10))
-    email = db.Column(db.String(100))
-    phone_number = db.Column(db.String(20))#!/usr/bin/env python
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import IntegrityError
-import os
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///people.db'
-db = SQLAlchemy(app)
-
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer)
 
     def __init__(self, name, age):
         self.name = name
@@ -43,33 +27,30 @@ def handle_integrity_error(error=None):
 
 @app.route('/api', methods=['POST'])
 def add_person():
-    data_list = request.get_json()
+    data = request.get_json()
 
-    if not isinstance(data_list, list):
-        return jsonify({'Response Error Message': "Expected a JSON list with 'name' and 'age' for each person."}), 400
+    if data is None:
+        return jsonify({'Response Error Message': 'Invalid JSON data. Expected a JSON object with "name" and "age" attributes.'}), 400
 
-    for data in data_list:
-        name = data.get('name')
-        age = data.get('age')
+    name = data.get('name')
+    age = data.get('age')
 
-        if not name or not age:
-            return jsonify({'Response Error Message': 'Name and age are required for each person'}), 400
+    if not name or not age:
+        return jsonify({'Response Error Message': 'Name and age are required for each person'}), 400
 
-        try:
-            person = Person(
-                name=name,
-                age=age,
-            )
-            db.session.add(person)
-            db.session.commit()
-        except IntegrityError:
-            db.session.rollback()
-            return jsonify({'Response Error Message': 'Database integrity error. Check your input data.'}), 400
+    try:
+        person = Person(
+            name=name,
+            age=age,
+        )
+        db.session.add(person)
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({'Response Error Message': 'Database integrity error. Check your input data.'}), 400
 
-    return jsonify({'Successful Response Message': 'Data added successfully'}), 201
+    return jsonify({'Successful Response Message': 'Person added successfully'}), 201
 
-
-# GET: Retrieve a list of all persons
 @app.route('/api', methods=['GET'])
 def get_all_persons():
     persons = Person.query.all()
@@ -80,7 +61,6 @@ def get_all_persons():
     } for person in persons]
     return jsonify({'persons': person_list})
 
-# READ: Fetch details of a person by ID
 @app.route('/api/<int:user_id>', methods=['GET'])
 def get_person(user_id):
     person = Person.query.get(user_id)
@@ -92,7 +72,6 @@ def get_person(user_id):
         'age': person.age,
     })
 
-# UPDATE: Modify details of an existing person by ID
 @app.route('/api/<int:user_id>', methods=['PUT'])
 def update_person(user_id):
     person = Person.query.get(user_id)
@@ -114,7 +93,6 @@ def update_person(user_id):
     except IntegrityError:
         return handle_integrity_error()
 
-# DELETE: Remove a person by ID
 @app.route('/api/<int:user_id>', methods=['DELETE'])
 def delete_person(user_id):
     person = Person.query.get(user_id)
@@ -128,7 +106,6 @@ def delete_person(user_id):
     except IntegrityError:
         return handle_integrity_error()
 
-# Route for the root URL ("/")
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'Home': '''
